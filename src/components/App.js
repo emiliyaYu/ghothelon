5dad91a519d3da2f006b79a89a02441fdf601270
Emiliya Yusupova <75200265+emiliyaYu@users.noreply.github.com>
2022-04-06 23:51:14 +0500

    0.1.0 - initialization map

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Map from "./common/map";
+
+function App() {
+  return (
+    <div className="App">
+      <Map />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/common/map/MapControl.js
@@ -0,0 +1,15 @@
+import { useMap } from "react-leaflet";
+import L from 'leaflet';
+
+const MapControl = () => {
+    const map = useMap();
+    const southWest = map.unproject([300, 3300], map.getMaxZoom()); // высота
+    const northEast = map.unproject([3700, 770], map.getMaxZoom()); // ширина
+    const totalBounds = new L.LatLngBounds(southWest, northEast);
+    map.setMaxBounds(totalBounds);
+
+    return null;
+}
+
+export default MapControl;
+
